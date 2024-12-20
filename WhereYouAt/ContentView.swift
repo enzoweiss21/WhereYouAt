836a1ce18d265e2445575d38129eb40feae7d242
enzoweiss21 <enzoweiss2014@gmail.com>
2024-12-20 17:53:15 -0500

    Initial Commit

---
+++ WhereYouAt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhereYouAt
+//
+//  Created by Enzo Weiss on 12/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhereYouAt/WhereYouAtApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereYouAtApp.swift
+//  WhereYouAt
+//
+//  Created by Enzo Weiss on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WhereYouAtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
